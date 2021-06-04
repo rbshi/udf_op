7b660f48037a66d29eadaf3fdec363be6c952b13
Runbin Shi <runbin.shi@inf.ethz.ch>
2021-06-04 13:58:11 +0200

    Feat: auto kernel allocation
    - add kernel allocation algorithm
    - fix hmss_path_per_kernel bug for udf_olap

---
+++ cfg/cfg_gen_ml.py
@@ -0,0 +1,54 @@
+import numpy as np
+from numpy import genfromtxt
+import matplotlib.pyplot as plt
+import csv
+import math
+import colorsys
+import numpy as np
+import csv
+from eda import kernel_alloc
+import math
+import sys
+
+# main
+def main():
+    kernel_name = "krnl_udf_ml"
+    with open(kernel_name + '.csv', newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        next(spamreader)
+        for row in spamreader:
+            [idx, num_kernel, hbm_grouping, auto_alloc] = map(int, row[0:4])
+            cfg_filename = kernel_name + "_" + str(idx) + ".cfg"
+            cfgfile = open(cfg_filename, 'w')
+            cfgfile.write("[connectivity]\n")
+            for idx_kernel in range(num_kernel):
+                cfgfile.write("sp=" + kernel_name + "_" + str(idx_kernel + 1) + ".p_hbm:HBM[0:31]\n")
+            cfgfile.write("nk=" + kernel_name + ":" + str(num_kernel) + "\n")
+            if auto_alloc:
+                slr_dense = float(row[4])
+                n_slr = kernel_alloc(kernel_name, num_kernel, slr_dense)
+                n_slr_presum = np.cumsum(n_slr)
+
+                for idx_kernel in range(num_kernel):
+                    compare_flag = np.tile(idx_kernel, n_slr_presum.shape) < n_slr_presum
+                    i_slr = np.where(compare_flag == True)[0][0]
+                    wr_line = "slr=" + kernel_name + "_" + str(idx_kernel+1) + ":SLR" + str(i_slr) + "\n"
+                    cfgfile.write(wr_line)
+
+            cfgfile.write("[vivado]\n")
+            vivado_cfg_lines = \
+                ["param=route.enableGlobalHoldIter=true\n", \
+                 "param=project.writeIntermediateCheckpoints=false\n", \
+                 "prop=run.impl_1.STEPS.PLACE_DESIGN.ARGS.DIRECTIVE=SSI_SpreadLogic_high\n", \
+                 "prop=run.impl_1.{STEPS.PHYS_OPT_DESIGN.IS_ENABLED}=true\n", \
+                 "prop=run.impl_1.{STEPS.PHYS_OPT_DESIGN.ARGS.MORE OPTIONS}={-slr_crossing_opt -tns_cleanup}\n", \
+                 "prop=run.impl_1.{STEPS.ROUTE_DESIGN.ARGS.MORE OPTIONS}={-ultrathreads}\n", \
+                 "prop=run.impl_1.{STEPS.POST_ROUTE_PHYS_OPT_DESIGN.IS_ENABLED}=true\n", \
+                 "prop=run.impl_1.{STEPS.POST_ROUTE_PHYS_OPT_DESIGN.ARGS.MORE OPTIONS}={-critical_cell_opt -rewire -hold_fix -sll_reg_hold_fix -retime}"]
+            cfgfile.writelines(vivado_cfg_lines)
+
+            cfgfile.close()
+
+
+if __name__ == '__main__':
+	main()
---
+++ cfg/eda.py
@@ -0,0 +1,66 @@
+import numpy as np
+import sys
+
+# kernel auto allocation
+def kernel_alloc(kernel_name, num_kernel, slr_dense, hmss_path_per_kernel=1):
+
+    # const, resource of each SLR (CLB_LUT, CLB_REG, BRAM, URAM, DSP)
+    slr_resource = np.array([
+        [439076, 879484, 488, 320, 2733],
+        [431922, 863836, 487, 320, 2877],
+        [431869, 864105, 512, 320, 2880]
+    ])
+
+    hmss_path_unit_resource = np.array([
+        # CLB_LUT
+        [[2057, 790, 1200],
+         [0, 2315,0],
+         [0, 0, 2577]],
+        # CLB_REG
+        [[3731, 2950, 2950],
+         [0, 5427, 2500],
+         [0, 0, 5421]]]
+    )
+
+    hmss_path_switch = np.array([
+        [0, 0],
+        [7438, 17034],
+        [0, 0]
+    ])
+
+    # const, single kernel resource (e.g., from HLS, or vivado->kernel.rpt)
+    kernel_resource={
+        "krnl_udf_selection":[8043,15352,15,16,0],
+        "krnl_udf_olap": [45828,48938,63,16,0],
+        "krnl_udf_ml": [44901,54673,23,40,205]
+    }
+
+    # initial value - assume all kernels are allocated in slr0
+    n_slr = np.array([num_kernel, 0, 0])
+    slr_resource_meet = [False, False, False]
+
+    # scheme: move a kernel to the next slr, if resource is NOT meet
+    for i_slr in range(3):
+        while ~slr_resource_meet[i_slr]:
+            hmss_path_clb = np.transpose(np.dot(hmss_path_unit_resource, n_slr * hmss_path_per_kernel))
+            hmss_path_all = hmss_path_clb + hmss_path_switch
+            kernel_all = np.transpose(np.outer(np.array(kernel_resource[kernel_name]), n_slr))
+            dynamic_all = kernel_all + np.concatenate((hmss_path_all, np.zeros([3, 3], dtype=int)), axis=1)
+            static_per_slr = np.array([31093, 42921, 0, 0, 0])
+            resource_all = dynamic_all + np.tile(static_per_slr, [3,1])
+
+            slr_resource_meet[i_slr] = (resource_all[i_slr, 0] < slr_resource[i_slr, 0] * slr_dense) & \
+                                       (resource_all[i_slr, 1] < slr_resource[i_slr, 1] * slr_dense) & \
+                                       (resource_all[i_slr, 2] <= slr_resource[i_slr, 2]) & \
+                                       (resource_all[i_slr, 3] <= slr_resource[i_slr, 3]) & \
+                                       (resource_all[i_slr, 4] <= slr_resource[i_slr, 4])
+
+            # last slr, but CANNOT meet
+            if (i_slr == 2) & ~slr_resource_meet[i_slr]:
+                print("[Error] Kernel allocation CANNOT meet the hardware resoruce constraint.")
+                sys.exit(1)
+                return n_slr
+
+            if ~slr_resource_meet[i_slr]:
+                n_slr[i_slr] -= 1; n_slr[i_slr+1] += 1
+    return n_slr
